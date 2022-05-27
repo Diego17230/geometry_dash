9541af9e302a8baeea67f71e9644ce7172cd3caf
Tanner <tannerelephant@gmail.com>
2022-05-27 09:33:11 -0700

    Added Moving Platform Added Clock

--- main.py
+++ main.py
@@ -15,16 +15,31 @@ class Player(pygame.sprite.Sprite):
         self.surf.fill((0, 255, 0))
 
 
+class Platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((50, 20))
+        self.rect = self.surf.get_rect(center=(800 - self.surf.get_width(), 250 - self.surf.get_height()))
+        self.surf.fill((255, 255, 0))
+
+    def update(self, dt):
+        self.rect.move_ip(-0.1 * dt, 0)
+
+
 class Game():
     def __init__(self):
+        self.clock = pygame.time.Clock()
         self.running = True
 
         self.player = Player()
+        self.platform = Platform()
 
         while self.running:
             self.update()
 
     def update(self):
+        dt = self.clock.tick(30)
+
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
@@ -32,8 +47,11 @@ class Game():
             elif event.type == QUIT:
                 self.running = False
 
+        self.platform.update(dt)
+
         screen.fill((100, 110, 110))
         screen.blit(self.player.surf, self.player.rect)
+        screen.blit(self.platform.surf, self.platform.rect)
         pygame.display.flip()
 
 
