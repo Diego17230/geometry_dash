9c91e9c54f75484e30699cf4f81ea5d954e8f8ac
Tanner <tannerelephant@gmail.com>
2022-06-09 18:52:13 -0700

    Fixed all the existing bugs and added lots of comments

--- game/main.py
+++ game/main.py
@@ -1,4 +1,6 @@
 import random
+import sys
+
 import pygame
 from pygame.locals import *
 
@@ -48,6 +50,12 @@ class Player(pygame.sprite.Sprite):
     def update(self, space, screen, platform_group):
         # Sets space (Jump) to whatever was inputted in the update function
         # Checks for collisions
+        """This function:
+         - Checks for collisions with platforms (on ground)
+         - Applies gravity to player
+         - Sees if player can/should jump
+         -
+        """
         collision = self.ground_collision_detector(platform_group)
         if collision:
             self.on_ground = True
@@ -73,15 +81,20 @@ class Player(pygame.sprite.Sprite):
     def move(self):
         self.rect.move_ip(*self.vel)
 
+    # Jump function
     def jump(self):
         self.vel[1] = -10
         self.on_ground = False
 
+    # Gravity function
     def gravity(self):
         self.vel[1] += .7
 
     # Collision detector
     def ground_collision_detector(self, platform_group):
+        """This function checks if the player collides with platforms:
+        Note: If it hits either side of the platform or the bottom it dies
+        """
         for platform in platform_group:
             if platform.rect.colliderect(self.rect):
                 if abs(self.rect.bottom - platform.rect.top) in range(1, 15):
@@ -134,6 +147,12 @@ class Platform(pygame.sprite.Sprite):
 
 class Game:
     def __init__(self, mode):
+        """
+        This function checks the mode and adds the appropriate features accordingly
+        """
+        # This is the mode which will effect how the game is player
+        # There are 3 modes: AI Only (0), Player Only (1), Player and AI (2)
+        # The mode is just one of the numbers listed above: 0, 1, or 2
         self.mode = mode
         self.clock = pygame.time.Clock()
         # Will run the game while this is true
@@ -149,7 +168,7 @@ class Game:
         self.all_sprites = pygame.sprite.Group(self.ground)
         self.score = 0
 
-        # Creates the player
+        # Checks the mode to see what needs to be in the game
         if mode != 1:
             self.AI = Player(200, 250)
             self.AI.add(self.all_sprites)
@@ -167,29 +186,31 @@ class Game:
         self.incoming_obstacles = []
 
         while self.running:
+            # Checks what mode the game should be played in and updates accordingly
             if mode == 0:
                 self.mode1_update()
             elif mode == 1:
                 self.mode2_update()
             elif mode == 2:
                 self.mode3_update()
-            
-            if mode != 0 and self.player.dead:
-                End(self.score)
 
+            # Checks if it's not the AI only mode and if the player is dead to move on to the end screen
             if mode != 0 and self.player.dead:
                 End(self.score)
-        pygame.quit()
 
     def update_score(self):
+        """This function updates the score of the Player/AI"""
+        # Adds 1 to the score when called (called every frame)
         self.score += 1
         font = pygame.font.Font('freesansbold.ttf', 32)
+        # Sets the text to the updated score
         text = font.render('Score: ' + str(self.score), True, (0, 255, 0))
 
         textRect = text.get_rect()
 
         # set the center of the rectangular object.
         textRect.center = (250, 100)
+        # Adds the text to the screen
         self.screen.blit(text, textRect)
 
     def check_distance(self, obj1, obj2):
@@ -200,6 +221,7 @@ class Game:
     def manage_obstacle_delay(self, spike_delay=61, platform_delay=60,
                               random_platforms=False):
         # Lowers the spike and platform delay by 1 every frame
+        # Basically a countdown for when the next spike/platform should come
         self.platform_delay -= 1
         self.spike_delay -= 1
 
@@ -222,21 +244,25 @@ class Game:
             self.spike_delay = spike_delay
 
     def update_obstacles(self, dt):
+        """This function takes in delta time (dt) to keep a constant rate of the game even if you lose frames"""
         # Updates every platform in the platform group (except the ground)
         for platform in self.platforms:
             if platform != self.ground:
+                # moves platforms with dt
                 platform.update(dt)
 
         # Updates every spike and checks for collisions with player
         for spike in self.spikes:
+            # moves spikes with dt
             spike.update(dt)
 
     def mode1_update(self):
         """This function is run every frame of the game and returns nothing"""
+        """This mode of the game is AI ONLY"""
         # Sets delta time to the clock tick (will help catch up if application lags)
         dt = self.clock.tick(30)
 
-        # Checks if there are two or more obstacles coming towards the player
+        # Checks if there are two or more obstacles coming towards the AI
         if len(self.incoming_obstacles) >= 2:
             # Gets the first two obstacles
             obstacle1 = self.incoming_obstacles[0]
@@ -314,7 +340,14 @@ class Game:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE or event.type == QUIT:
+                    # A little overkill on quitting the game perhaps
+                    # but everything is stopped
                     self.running = False
+                    pygame.quit()
+                    # More of a brute force method because there was a problem
+                    # with the pygame.font (Still not sure why)
+                    sys.exit()
+                    return
 
         self.update_obstacles(dt)
 
@@ -334,6 +367,7 @@ class Game:
 
     def mode2_update(self):
         """This function is run every frame of the game and returns nothing"""
+        """This mode is PLAYER ONLY"""
         # Sets delta time to the clock tick (will help catch up if application lags)
         dt = self.clock.tick(30)
 
@@ -345,12 +379,21 @@ class Game:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE or event.type == QUIT:
+                    # A little overkill on quitting the game perhaps
+                    # but everything is stopped
                     self.running = False
+                    pygame.quit()
+                    # More of a brute force method because there was a problem
+                    # with the pygame.font (Still not sure why)
+                    sys.exit()
+                    return
                 if event.key == K_SPACE:
                     self.space = True
 
+        # Updates obstacles
         self.update_obstacles(dt)
 
+        # Checks if player has collided with a spike
         for spike in self.spikes:
             if spike.rect.colliderect(self.player.rect):
                 self.player.dead = True
@@ -373,6 +416,7 @@ class Game:
 
     def mode3_update(self):
         """This function is run every frame of the game and returns nothing"""
+        """This mode is for PLAYER AND AI"""
         # Sets delta time to the clock tick (will help catch up if application lags)
         dt = self.clock.tick(30)
         self.score += 1
@@ -456,7 +500,14 @@ class Game:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE or event.type == QUIT:
+                    # A little overkill on quitting the game perhaps
+                    # but everything is stopped
                     self.running = False
+                    pygame.quit()
+                    # More of a brute force method because there was a problem
+                    # with the pygame.font (Still not sure why)
+                    sys.exit()
+                    return
                 if event.key == K_SPACE:
                     player_jump = True
 
@@ -508,6 +559,7 @@ class Menu:
             self.update()
 
     def update(self):
+        """This function runs every frame while this screen is on and returns nothing"""
         self.click_delay -= 1
         # blits the image onto the screen and then the button
         self.screen.blit(self.img_surf, [0, 0])
@@ -517,41 +569,51 @@ class Menu:
             if event.type == KEYDOWN:
                 # if the user quits
                 if event.type == pygame.QUIT or event.key == K_ESCAPE:
-                    pygame.quit()
                     self.running = False
+                    pygame.quit()
+                    sys.exit()
                     return
-        if pygame.mouse.get_pressed()[0] == 1:
-
-            mouse = pygame.mouse.get_pos()
+            # Checks if user is clicking down mouse
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Checks if user clicked on start_button
+                if self.start_button.rect.collidepoint(event.pos):
+                    self.running = False
+                    Game(self.modes.index(self.mode_button.text))
+                    return
+                # Checks if user clicked on mode_button (to change the mode)
+                elif self.mode_button.rect.collidepoint(event.pos) and self.click_delay < 0:
+                    # Click delay since it will rapid fire click otherwise
+                    self.click_delay = 30
+                    try:
+                        # Will try and go to next mode in list
+                        self.mode_button.set_text(
+                            self.modes[
+                                self.modes.index(self.mode_button.text) + 1])
+                    except IndexError:
+                        # If there is no more modes it will reset back to the first mode
+                        self.mode_button.set_text(self.modes[0])
             # If the user left clicks on the button the game class is called and this pygame is quit
-            if self.start_button.rect.collidepoint(mouse):
-                self.running = False
-                Game(self.modes.index(self.mode_button.text))
-                return
             # Changes the mode and blits the text onto the screen
-            elif self.mode_button.rect.collidepoint(
-                    mouse) and self.click_delay < 0:
-                self.click_delay = 30
-                try:
-                    self.mode_button.set_text(
-                        self.modes[
-                            self.modes.index(self.mode_button.text) + 1])
-                except IndexError:
-                    self.mode_button.set_text(self.modes[0])
-        pygame.display.update()
+        # Updates visuals of screen
+        pygame.display.flip()
 
 
 class End:
     def __init__(self, score):
         # Adds the text
         font = pygame.font.Font('freesansbold.ttf', 32)
+        # Addes the score to the screen
         self.text = font.render('Score: ' + str(score), True, (0, 255, 0))
         self.textRect = self.text.get_rect()
         # set the center of the rectangular object.
-        self.textRect.center = (425, 25)
+        # Moves the score over to the left more the higher the number is
+        self.textRect.center = (425 - (len(str(score)) * 10), 25)
 
-        self.died_button = Button("freesansbold.ttf", 40, "You Died!",
-                                  (255, 255, 255), (250, 125))
+        # Adds dead text
+        self.dead_text = font.render('You Died!', True, (255, 255, 255))
+        self.dead_textRect = self.dead_text.get_rect()
+        self.dead_textRect.center = (250, 125)
+
         self.continue_button = Button("freesansbold.ttf", 40, "Continue?",
                                       (255, 255, 255), (250, 300))
         self.screen = pygame.display.set_mode((500, 500))
@@ -563,22 +625,29 @@ class End:
             self.update()
 
     def update(self):
+        """This function runs every frame while this screen is on and returns nothing"""
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 # if the user quits
                 if event.type == pygame.QUIT or event.key == K_ESCAPE:
                     self.running = False
-        if pygame.mouse.get_pressed()[0] == 1:
-            mouse = pygame.mouse.get_pos()
-            # If the user left clicks on the button the game class from the main.py file is called and this
-            if self.continue_button.rect.collidepoint(mouse):
-                self.running = False
-                Menu()
-                return
+                    pygame.quit()
+                    sys.exit()
+                    return
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # Checks if user clicks continue button
+                if self.continue_button.rect.collidepoint(event.pos):
+                    self.running = False
+                    # Runs menu
+                    Menu()
+                    return
+
+        # Puts background, buttons, and text on screen
         self.screen.blit(self.img_surf, [0, 0])
-        self.screen.blit(self.died_button.surf, self.died_button.rect)
+        self.screen.blit(self.dead_text, self.dead_textRect)
         self.screen.blit(self.continue_button.surf, self.continue_button.rect)
         self.screen.blit(self.text, self.textRect)
+        # Shows the new screen
         pygame.display.flip()
 
 
